9acd0b350f9fbc22cdafb0e587515b2af5a0f27c
zoharb19 <zoharb19@gmail.com>
2022-12-07 15:43:07 +0200

    fixed guessing game in lesson_03

--- homework/main.py
+++ homework/main.py
@@ -2,18 +2,29 @@ import random
 
 
 def main():
-    random_num = random.randint(0, 10)
-    print("this is guess the number game")
-    guess = int(input())
-    for guess_taken in (0, 5):
-        print("guess a number: " + str(guess))
-        if guess < random_num:
-            print("number too low")
-        elif guess > random_num:
-            print("guess is too high")
-        else:
+    max_num = 10
+    # 1. generate random number
+    random_num = random.randint(0, max_num)
+    # 2. loop for 5 times:
+    for try_num in range(10):
+        # 2.1 ask player to choose a number in range
+        print(f"choose a number between 0 to {max_num}")
+        # 2.2 player insert input
+        player_num = int(input())
+        # 2.3 if right:
+        if player_num == random_num:
+            # 2.3.1 print you won
+            print("YOU WON")
+        # 2.3.2 exit game
             break
-    
+        # 2.4 else:
+        else:
+            # 2.4.1 print if higher or lower
+            if player_num > random_num:
+                print("your number is too high")
+            else:
+                print("your number is too low")
+
 
 if __name__ == '__main__':
     main()
