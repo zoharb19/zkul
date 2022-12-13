860b9394830069a4a3d9c7884e72499bca59a772
zoharb19 <zoharb19@gmail.com>
2022-12-13 11:51:59 +0200

    renamed vars to full name and deleted 'else sys.exit'

--- lesson_04/RockPaperScissors/main.py
+++ lesson_04/RockPaperScissors/main.py
@@ -3,61 +3,59 @@ import sys
 
 
 def main():
-    r = "rock"
-    p = "paper"
-    s = "scissors"
-    rps = [r, p, s]
+    rock = "rock"
+    paper = "paper"
+    scissors = "scissors"
+    rock_paper_scissors = [rock, paper, scissors]
 
     print("rock paper scissors!")
     print("choose your weapon: Rock, Paper, Scissors or Q to quit:")
-    user_inp = input()
-    pc_inp = random.choice(rps)
+    user_input = input()
+    pc_input = random.choice(rock_paper_scissors)
 
-    while user_inp != "q":
-        if user_inp == pc_inp:
-            print(f"{user_inp} vs {pc_inp}")
+    while user_input != "q":
+        if user_input == pc_input:
+            print(f"{user_input} vs {pc_input}")
             print("its a tie")
             print("choose your weapon: Rock, Paper, Scissors or Q to quit:")
-            user_inp = input()
-            pc_inp = random.choice(rps)
-        elif user_inp == r and pc_inp == p:
-            print(f"{user_inp} vs {pc_inp}")
+            user_input = input()
+            pc_input = random.choice(rock_paper_scissors)
+        elif user_input == rock and pc_input == paper:
+            print(f"{user_input} vs {pc_input}")
             print("pc won")
             print("choose your weapon: Rock, Paper, Scissors or Q to quit:")
-            user_inp = input()
-            pc_inp = random.choice(rps)
-        elif user_inp == r and pc_inp == s:
-            print(f"{user_inp} vs {pc_inp}")
+            user_input = input()
+            pc_input = random.choice(rock_paper_scissors)
+        elif user_input == rock and pc_input == scissors:
+            print(f"{user_input} vs {pc_input}")
             print("you won")
             print("choose your weapon: Rock, Paper, Scissors or Q to quit:")
-            user_inp = input()
-            pc_inp = random.choice(rps)
-        elif user_inp == p and pc_inp == r:
-            print(f"{user_inp} vs {pc_inp}")
+            user_input = input()
+            pc_input = random.choice(rock_paper_scissors)
+        elif user_input == paper and pc_input == rock:
+            print(f"{user_input} vs {pc_input}")
             print("you won")
             print("choose your weapon: Rock, Paper, Scissors or Q to quit:")
-            user_inp = input()
-            pc_inp = random.choice(rps)
-        elif user_inp == p and pc_inp == s:
-            print(f"{user_inp} vs {pc_inp}")
+            user_input = input()
+            pc_input = random.choice(rock_paper_scissors)
+        elif user_input == paper and pc_input == scissors:
+            print(f"{user_input} vs {pc_input}")
             print("pc won")
             print("choose your weapon: Rock, Paper, Scissors or Q to quit:")
-            user_inp = input()
-            pc_inp = random.choice(rps)
-        elif user_inp == s and pc_inp == p:
-            print(f"{user_inp} vs {pc_inp}")
+            user_input = input()
+            pc_input = random.choice(rock_paper_scissors)
+        elif user_input == scissors and pc_input == paper:
+            print(f"{user_input} vs {pc_input}")
             print("you won")
             print("choose your weapon: Rock, Paper, Scissors or Q to quit:")
-            user_inp = input()
-            pc_inp = random.choice(rps)
-        elif user_inp == s and pc_inp == r:
-            print(f"{user_inp} vs {pc_inp}")
+            user_input = input()
+            pc_input = random.choice(rock_paper_scissors)
+        elif user_input == scissors and pc_input == rock:
+            print(f"{user_input} vs {pc_input}")
             print("pc won")
             print("choose your weapon: Rock, Paper, Scissors or Q to quit:")
-            user_inp = input()
-            pc_inp = random.choice(rps)
-        else:
-            sys.exit()
+            user_input = input()
+            pc_input = random.choice(rock_paper_scissors)
 
 
 if __name__ == '__main__':
