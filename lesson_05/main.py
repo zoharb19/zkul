5b07e777decb8b08ee360070bdd5a00cba04d872
zoharb19 <zoharb19@gmail.com>
2022-12-16 16:47:57 +0200

    renamed q to full name 'quit_game'

--- lesson_05/main.py
+++ lesson_05/main.py
@@ -6,7 +6,7 @@ def report_winner(winner):
 
 
 def main():
-    q = "q"
+    quit_game = "q"
     rock = "rock"
     paper = "paper"
     scissors = "scissors"
@@ -15,16 +15,16 @@ def main():
     print("rock paper scissors!")
     user_input = None
 
-    while user_input != q:
+    while user_input != quit_game:
         print("choose your weapon: Rock, Paper, Scissors or Q to quit:")
         user_input = input()
         pc_input = random.choice(rock_paper_scissors)
-        if user_input == q:
+        if user_input == quit_game:
             print("bye")
         elif user_input != pc_input or user_input == pc_input:
             print(f"{user_input} vs {pc_input}")
 
-        if user_input == q:
+        if user_input == quit_game:
             break
         elif user_input == pc_input:
             report_winner("no one")
