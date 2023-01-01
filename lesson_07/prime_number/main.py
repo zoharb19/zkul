6b6479f652879f9ca6fd623d5ff646382c4c1403
zoharb19 <zoharb19@gmail.com>
2023-01-01 21:18:13 +0200

    def return True and False

--- lesson_07/prime_number/main.py
+++ lesson_07/prime_number/main.py
@@ -3,17 +3,19 @@ def primes(a_number):
     # prime number only divides by itself and 1
     for i in range(2, a_number):
         if a_number % i == 0:
-            return "not a prime"
+            return False
         else:
             i = i + 1
             if a_number == i:
-                return "a prime"
+                return True
 
 
 def main():
-    print("choose a num")
     num = int(input())
-    print(primes(num))
+    if primes(num) is False:
+        print(f"{num} is not a prime number")
+    else:
+        print(f"{num} is a prime number")
 
 
 if __name__ == '__main__':
