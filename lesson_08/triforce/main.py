2cde8a1bf6dca48214f0b87fd5b207ede81810f8
zoharb19 <zoharb19@gmail.com>
2023-01-05 20:21:25 +0200

    pretty sure no changes were added

--- lesson_08/triforce/main.py
+++ lesson_08/triforce/main.py
@@ -1,13 +1,13 @@
 
 def main():
-    rows = 6
+    rows = 10
     columns = 1
-    for i in range(1, rows + 1):
+    for i in range(1, rows):
         for j in range(1, columns + 1):
-            print(j - 1, end="")
+            print(j, end="")
         print()
         columns = columns + 1
-
+        
 
 if __name__ == '__main__':
     main()
