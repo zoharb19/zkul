5cc18111e9027680bc9ac5313c505f47a6de8801
zoharb19 <zoharb19@gmail.com>
2023-01-05 18:42:38 +0200

    made a simple table of multiplication

--- lesson_08/main.py
+++ lesson_08/main.py
@@ -2,12 +2,13 @@
 
 
 def main():
+    current = table_of_multiplication
+    current()
+
+
+def table_of_multiplication():
     rows = 10
     columns = 10
-    table_of_multiplication(rows, columns)
-
-
-def table_of_multiplication(rows, columns):
     for i in range(1, rows + 1):
         for j in range(1, columns + 1):
             print(f'{i * j:02d}', end=" ")
