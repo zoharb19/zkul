340efc1afc3e2dbf4ad42b665ff217f780393968
zoharb19 <zoharb19@gmail.com>
2023-01-05 15:58:17 +0200

    created program for table of multiplication

---
+++ lesson_08/main.py
@@ -0,0 +1,19 @@
+#nested loops!
+
+
+def main():
+    rows = 10
+    columns = 10
+    table_of_multiplication(rows, columns)
+
+
+def table_of_multiplication(rows, columns):
+    for i in range(1, rows + 1):
+        for j in range(1, columns + 1):
+            print(f'{i * j:02d}', end=" ")
+        print()
+
+
+if __name__ == '__main__':
+    main()
+
