a8bf20c13034b077bb36bdd621f2bb8a8ea0d3c6
zoharb19 <zoharb19@gmail.com>
2023-01-07 15:22:56 +0200

    made the holy triforce

--- lesson_08/triforce/main.py
+++ lesson_08/triforce/main.py
@@ -1,13 +1,20 @@
-
-def main():
-    rows = 10
-    columns = 1
+def triforce(columns, rows):
     for i in range(1, rows):
         for j in range(1, columns + 1):
             print(j, end="")
         print()
         columns = columns + 1
-        
+        if i == rows - 1:
+            for i in range(1, rows + 1):
+                for j in range(1, columns - 1):
+                    print(j, end="")
+                print()
+                columns -= 1
+
+
+def main():
+    triforce(1, 105)
+
 
 if __name__ == '__main__':
     main()
