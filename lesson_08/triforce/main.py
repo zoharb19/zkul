d03b9b2bcff298dad35010f27b471031ff1df623
zoharb19 <zoharb19@gmail.com>
2023-01-11 17:23:34 +0200

    for i loop is now k loop

--- lesson_08/triforce/main.py
+++ lesson_08/triforce/main.py
@@ -1,19 +1,18 @@
 def triforce(columns, rows):
     for i in range(1, rows):
         for j in range(1, columns + 1):
-            print(j, end="")
+            print(j, end=" ")
         print()
         columns = columns + 1
-        if i == rows - 1:
-            for i in range(1, rows + 1):
-                for j in range(1, columns - 1):
-                    print(j, end="")
-                print()
-                columns -= 1
+    for k in range(1, rows + 1):
+        for l in range(1, columns - 1):
+            print(l, end=" ")
+        print()
+        columns -= 1
 
 
 def main():
-    triforce(1, 105)
+    triforce(1, 6)
 
 
 if __name__ == '__main__':
